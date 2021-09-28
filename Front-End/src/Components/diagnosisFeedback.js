d1e96eb2dad556c1305ae0d67fa4baec359a707a
Rafiq <whitleyrafiq95@gmail.com>
2021-09-28 10:58:49 -0400

    feedback component for if patient has recieved diagnosis info scalpted

--- Front-End/src/Components/diagnosisFeedback.js
+++ Front-End/src/Components/diagnosisFeedback.js
@@ -1,17 +1,43 @@
+import React, {useState} from "react";
 import { bindActionCreators } from 'redux';
 import { actionCreators } from "../store/index";
 import {useHistory} from 'react-router-dom';
-import { useDispatch } from "react-redux"; 
-const diagnosisFeedback = () => {
+import { useDispatch } from "react-redux";
+
+
+const DiagnosisFeedback = () => {
         const dispatch = useDispatch()
         const {giveDiagnosis} = bindActionCreators(actionCreators,dispatch);
-        let history = useHistory();
+        const history = useHistory();
+        const [feedbackValue, setFeedback] = useState(false);
+
         return (
             <div>
-
+                <select id='hadFeedback' onChange={() => {
+                    const value = document.querySelector('#hadFeedback');
+                    setFeedback(!feedbackValue)
+                }}>
+                    <option disabled selected value> -- select an option -- </option>
+                    <option>Yes</option>
+                    <option>No</option>
+                </select>
+                {feedbackValue 
+                ? 
+                  <div>
+                    <label>Please Enter Feedback Below</label>
+                    <br/>
+                    <textarea id="feedback" rows="4" cols="50" />
+                    <br />
+                    <button onClick={() => {
+                        const feedback = document.querySelector('#feedback').value;
+                        console.log(feedback)
+                    }} > Submit </button>
+                
+                  </div>
+                : null
+                }
             </div>
         )
-    )
 }
 
-export default diagnosisFeedback;
+export default DiagnosisFeedback;
