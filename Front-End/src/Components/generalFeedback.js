280dd7610a63b975d3b318acbc9c1a1115565713
Rafiq <whitleyrafiq95@gmail.com>
2021-09-28 13:10:44 -0400

    general feedbackk component added now routes need to be connected

---
+++ Front-End/src/Components/generalFeedback.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import { bindActionCreators } from 'redux';
+import { actionCreators } from "../store/index";
+import {useHistory} from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import getDiagnosis from "../methods/getDiagnosis";
+
+const GeneralFeedBack = () => {
+        const dispatch = useDispatch()
+        const {giveFeedback} = bindActionCreators(actionCreators,dispatch);
+        const history = useHistory();
+        return (
+            <div>
+               <label>We appreciate the feedback, one last question: how do you feel about being diagnosed with {getDiagnosis()}?</label>
+               <br />
+               <textarea id="feedback" rows="4" cols="50" />
+               <br />
+               <button onClick={() => {
+                   let feedback = document.querySelector('#feedback').value;
+               }}>Submit</button>
+            </div> 
+
+        )
+}
+
+export default GeneralFeedBack;
